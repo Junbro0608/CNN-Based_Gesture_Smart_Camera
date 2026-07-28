2c218644f5f5f06e92c83f2df02c87515f59706b
junbro <wnsgud1235686@gmail.com>
2026-07-28 10:58:58 +0900

    Lab00_instunce

--- Lab00_Dram_controller/Dram_controller/Dram_controller.srcs/sources_1/CNN/CNN_accelerator.sv
+++ Lab00_Dram_controller/Dram_controller/Dram_controller.srcs/sources_1/CNN/CNN_accelerator.sv
@@ -4,14 +4,23 @@ module CNN_accelerator #(
     parameter DATA_ADDR_WIDTH = 126 * 126 * 16,
     parameter DATA_DATA_WIDTH = 8
 ) (
-    input  logic clk,
-    input  logic rst_n,
-    input  logic done,
-    input  logic busy,
+    input logic clk,
+    input logic rst_n,
+    input logic start,
+    output logic done,
+    output logic busy,
     output logic result,
     //push img
     output logic img_raddr,
-    input  logic img_rdata
+    input logic img_rdata,
+    output logic [71:0] s_axis_mms2s_cmd_tdata,  // 명령어 tdata
+    input logic s_axis_mms2s_cmd_tready,  // 명령어 tready
+    output logic s_axis_mms2s_cmd_tvalid,  // 명령어 tvalid
+    input logic [63:0] s_axis_mm2s_tdata,  //rdata push data
+    input  logic [ 7:0] s_axis_mm2s_tkeep,        //rdata 64비트(8바이트) 중에서 몇 번째 바이트가 유효한지*무시*
+    input logic s_axis_mm2s_tlast,  //rdata 마지막 데이터라는 신호
+    output logic s_axis_mm2s_tready,  //rdata tready
+    input logic s_axis_mm2s_tvalid  //rdata push done
 );
 
     CNN_acc_controller U_CNN_acc_controller (
@@ -19,23 +28,6 @@ module CNN_accelerator #(
         .rst_n(rst_n)
     );
 
-    //Weigth(20,480 bytes)
-    pingpongBuffer #(
-        .ADDR_WIDTH(WT_ADDR_WIDTH),
-        .DATA_WIDTH(WT_DATA_WIDTH)
-    ) U_wight_Buffer (
-        //buffer ctrl
-        .w_sel(WT_w_sel),
-        //write
-        .wclk (clk),
-        .we   (WT_we),
-        .wAddr(WT_wAddr),
-        .wData(WT_wData),
-        //read
-        .rclk (clk),
-        .rAddr(WT_rAddr),
-        .rData(WT_rData)
-    );
 
     conv #(
         .WT_ADDR_WIDTH  (WT_ADDR_WIDTH),
@@ -109,7 +101,50 @@ module CNN_accelerator #(
         .rData(DB_rData)
     );
 
+    dram_controller u_dram_controller (
+        .clk  (clk),
+        .rst_n(rst_n)
+    );
+
+    //Weigth(20,480 bytes)
+    pingpongBuffer #(
+        .ADDR_WIDTH(WT_ADDR_WIDTH),
+        .DATA_WIDTH(WT_DATA_WIDTH)
+    ) U_wight_Buffer (
+        //buffer ctrl
+        .w_sel(WT_w_sel),
+        //write
+        .wclk (clk),
+        .we   (WT_we),
+        .wAddr(WT_wAddr),
+        .wData(WT_wData),
+        //read
+        .rclk (clk),
+        .rAddr(WT_rAddr),
+        .rData(WT_rData)
+    );
+
 
 
 
 endmodule
+
+module mux3 #(
+    parameter WIDTH = 1
+) (
+    input  logic [WIDTH-1:0] in0,
+    input  logic [WIDTH-1:0] in1,
+    input  logic [WIDTH-1:0] in2,
+    input  logic [1:0]       sel,
+    output logic [WIDTH-1:0] out
+);
+    always_comb begin
+        case (sel)
+            2'd0: out = in0;
+            2'd1: out = in1;
+            2'd2: out = in2;
+            default: out = in0;
+        endcase
+    end
+endmodule
+
--- Lab00_Dram_controller/Dram_controller/Dram_controller.srcs/sources_1/top.sv
+++ Lab00_Dram_controller/Dram_controller/Dram_controller.srcs/sources_1/top.sv
@@ -1,50 +1,58 @@
 module top (
-    input  logic        clk,
-    input  logic        rst_n,
+    input logic clk,
+    input logic rst_n,
     // --------------------Pcam------------------------
     // 1. MIPI D-PHY High-Speed (HS) 라인 (카메라 -> FPGA)
-    input  logic        dphy_hs_clock_clk_p,
-    input  logic        dphy_hs_clock_clk_n,
-    input  logic [ 1:0] dphy_data_hs_p,
-    input  logic [ 1:0] dphy_data_hs_n,
+    input logic dphy_hs_clock_clk_p,
+    input logic dphy_hs_clock_clk_n,
+    input logic [1:0] dphy_data_hs_p,
+    input logic [1:0] dphy_data_hs_n,
     // 2. MIPI D-PHY Low-Power (LP) 라인 (카메라 -> FPGA)
-    input  logic        dphy_clk_lp_p,
-    input  logic        dphy_clk_lp_n,
-    input  logic [ 1:0] dphy_data_lp_p,
-    input  logic [ 1:0] dphy_data_lp_n,
+    input logic dphy_clk_lp_p,
+    input logic dphy_clk_lp_n,
+    input logic [1:0] dphy_data_lp_p,
+    input logic [1:0] dphy_data_lp_n,
     // 3. 카메라 제어 및 시스템 신호 (FPGA -> 카메라)
-    output logic        cam_clk,              // 카메라 센서 구동용 클럭
-    output logic        cam_gpio,             // 카메라 리셋(Reset) 또는 전원 제어
+    output logic cam_clk,  // 카메라 센서 구동용 클럭
+    output logic cam_gpio,  // 카메라 리셋(Reset) 또는 전원 제어
     // 4. I2C (SCCB) 설정 통신 라인
-    inout  logic        cam_scl,              // I2C 클럭 (Clock)
-    inout  logic        cam_sda,              // I2C 데이터 (Data)
+    inout logic cam_scl,  // I2C 클럭 (Clock)
+    inout logic cam_sda,  // I2C 데이터 (Data)
     // ---------------------AXI2DRAM-----------------------
-    output logic [71:0] S_AXIS_MM2S_CMD,
-    input  logic [63:0] M_AXIS_MM2S
+    output logic [71:0] s_axis_mms2s_cmd_tdata,  // 명령어 tdata
+    input logic s_axis_mms2s_cmd_tready,  // 명령어 tready
+    output logic s_axis_mms2s_cmd_tvalid,  // 명령어 tvalid
+    input logic [63:0] s_axis_mm2s_tdata,  //rdata push data
+    input  logic [ 7:0] s_axis_mm2s_tkeep,        //rdata 64비트(8바이트) 중에서 몇 번째 바이트가 유효한지*무시*
+    input logic s_axis_mm2s_tlast,  //rdata 마지막 데이터라는 신호
+    output logic s_axis_mm2s_tready,  //rdata tready
+    input logic s_axis_mm2s_tvalid  //rdata push done
 );
 
-    dram_controller U_dram_ctroller (
-        .clk              (),  // 125MHz 시스템 클럭
-        .rst_n            (),  // Active-Low 리셋 (Processor System Reset에서 옴)
-        // CNN 메인 제어부에서 받는 신호
-        .start_req        (),  // 데이터 요청 트리거 (1 클럭 펄스)
-        .sys_addr         (),  // 읽어올 메모리 시작 주소 (예: 0x1000_0000)
-        .sys_btt          (),  // 읽어올 바이트 크기 (예: 128)
-        // DataMover의 S_AXIS_MM2S_CMD 포트로 나가는 신호 (AXI Stream Master)
-        .m_axis_cmd_tdata (),
-        .m_axis_cmd_tvalid(),
-        .m_axis_cmd_tready()
-    );
 
     CNN U_CNN (
-        .clk  (clk),
+        .clk(clk),
         .rst_n(rst_n),
         .start(start),
-        .done (),
-        .busy ()
+        .done(done),
+        .busy(busy),
+        //Dram
+        .s_axis_mms2s_cmd_tdata (s_axis_mms2s_cmd_tdata),
+        .s_axis_mms2s_cmd_tready(s_axis_mms2s_cmd_tready),
+        .s_axis_mms2s_cmd_tvalid(s_axis_mms2s_cmd_tvalid),
+        .s_axis_mms2s_cmd_tdata(s_axis_mms2s_cmd_tdata),  // 명령어 tdata
+        .s_axis_mms2s_cmd_tready(s_axis_mms2s_cmd_tready),  // 명령어 tready
+        .s_axis_mms2s_cmd_tvalid(s_axis_mms2s_cmd_tvalid),  // 명령어 tvalid
+        .s_axis_mms2s_tdata(s_axis_mms2s_tdata),  // 명령어 tdata
+        .s_axis_mms2s_tready(s_axis_mms2s_tready),  // 명령어 tready
+        .s_axis_mms2s_tvalid(s_axis_mms2s_tvalid),  // 명령어 tvalid
+        .s_axis_mm2s_tdata(s_axis_mm2s_tdata),  //rdata push data
+        .s_axis_mm2s_tlast(s_axis_mm2s_tlast),  //rdata 마지막 데이터라는 신호
+        .s_axis_mm2s_tready(s_axis_mm2s_tready),  //rdata tready
+        .s_axis_mm2s_tvalid(s_axis_mm2s_tvalid)  //rdata push done
     );
 
-    
+
 
 
 
